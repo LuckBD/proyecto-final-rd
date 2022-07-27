8340d62992ea27170523ac0b3322ceea8c644dbb
LuckBD <lucasben@hotmail.com>
2022-07-27 08:45:09 -0300

    casi

--- contact.js
+++ contact.js
@@ -1,4 +1,12 @@
-function myFunction() {
+function myFunction(){
     var element = document.body;
     element.classList.toggle("dark-mode");
   }
+
+function validateForm(){
+  let x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
